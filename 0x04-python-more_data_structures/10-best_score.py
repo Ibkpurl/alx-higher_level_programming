cad011ac1a6dbae2285a38d76fedb15e823c0beb
Paul Ibikunle <42482454+Ibkpurl@users.noreply.github.com>
2022-12-07 22:53:01 +0100

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+
+
 def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary) is 0:
-        return None
-    one_loop = True
-    for people in a_dictionary:
-        if one_loop:
-            best = people
-            score = a_dictionary[people]
-            one_loop = False
-        if a_dictionary[people] > score:
-            score = a_dictionary[people]
-            best = people
-    return(best)
+    """
+    gets the best value from a dictionary (greatest integer)
+    """
+    max_val = 0
+    winner = None
+    if type(a_dictionary) is dict:
+        for key, value in a_dictionary.items():
+            if value > max_val:
+                max_val = value
+                winner = key
+    return winner
