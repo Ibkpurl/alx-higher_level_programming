17b91a35d2c54d87045bba57e24a0fd8aab91336
Paul Ibikunle <42482454+Ibkpurl@users.noreply.github.com>
2022-12-07 22:55:46 +0100

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
+
+
 def roman_to_int(roman_string):
-    if roman_string is None or isinstance(roman_string, str) is False:
-        return None
-    n = 0
-    roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    for i in roman_string:
-        n += roman.get(i, 0)
-    subs = {'CM': -200, 'CD': -200, 'XC': -20, 'XL': -20, 'IX': -2, 'IV': -2}
-    chars = ""
-    for i in range(0, len(roman_string) - 1):
-        chars = roman_string[i] + roman_string[i + 1]
-        if chars in subs:
-            n += subs[chars]
-    return n
+    """
+    converts a roman numeral character into the respective integer
+    """
+    if roman_string is None or type(roman_string) is not str:
+        return (0)
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                  'D': 500, 'M': 1000}
+    roman_list = list(roman_string.upper())
+    result = 0
+    prev = 0
+    for letter in roman_list:
+        if letter in roman_dict:
+            result += roman_dict[letter]
+            if roman_dict[letter] > prev:
+                result -= prev * 2
+            prev = roman_dict[letter]
+        else:
+            return (0)
+    return (result)
