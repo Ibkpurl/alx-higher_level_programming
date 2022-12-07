d5ac69ae79242a39c51891d1ad17d86143f16e5a
Paul Ibikunle <42482454+Ibkpurl@users.noreply.github.com>
2022-12-07 22:46:13 +0100

    task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    return set_1 & set_2
 
-'''
-    set_3 = set_1 & set_2
-    return set_3
-'''
+
+def common_elements(set_1, set_2):
+    """
+    returns a set of common elements from two sets
+    """
+    return (set_1 & set_2)
