002b5e1d579c86ef9e11b331bcff3aff0777d91f
Paul Ibikunle <42482454+Ibkpurl@users.noreply.github.com>
2022-12-07 22:33:47 +0100

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for row in matrix:
-        new_row = list([x**2 for x in row])
-        new_matrix.append(new_row)
-    return(new_matrix)
 
-'''
-return[[n**2 for n in row] for row in matrix]
-'''
+
+def square_matrix_simple(matrix=[]):
+    """
+    Compute the square value of all integers of a matrix
+    """
+    return ([[(x**2) for x in row] for row in matrix])
